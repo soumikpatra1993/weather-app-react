174a559462ea0f39ba269a17deee1ec5ea6b86ee
ANINDITA CHATTOPADHYAY <anindita.2k18@gmail.com>
2019-03-17 19:44:46 +0530

    first commit

---
+++ src/component/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render () {
+        return (
+            <div className ='form-wrapper'>
+     <div>Form component</div>
+            </div>
+        )
+    }
+
+}
+export default Form
